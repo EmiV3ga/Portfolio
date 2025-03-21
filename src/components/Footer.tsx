1385b50f9bbf342ba59174d70606113401991064
Emiliano Vega <emilianodmvega@gmail.com>
2025-03-21 17:02:03 -0300

    remove commented-out code in Footer component for cleaner implementation

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,12 +6,9 @@ const Footer = () => {
     <footer className="py-8 mt-16 border-t border-gray-200 dark:border-gray-800">
       <div className="container mx-auto px-4">
         <div className="flex flex-col md:flex-row justify-between items-center">
-          {/* Texto del footer */}
           <div className="text-sm text-gray-600 dark:text-gray-300 mb-4 md:mb-0">
             © {new Date().getFullYear()} Emiliano Vega. All rights reserved.
           </div>
-
-          {/* Iconos de redes sociales */}
           <div className="flex space-x-6">
             <a
               href="https://github.com/EmiV3ga"
